5c0839f7782e19a1f732cdab146d0c40574f0c4d
Matt <mttspierings@gmail.com>
2016-09-24 18:16:22 +1200

    Version 2

--- js/main.ts
+++ js/main.ts
@@ -3,13 +3,18 @@ var go_btn_num = document.getElementById("num_btn");
 var num_result = document.getElementById("num_result");
 var usernum = document.getElementById("usernum");
 
-var go_btn_year = document.getElementById("");
+var go_btn_year = document.getElementById("year_btn");
+var useryear = document.getElementById("useryear");
+var year_result = document.getElementById("year_result");
+
 
 go_btn_num.addEventListener("click", function(){
     sendNumberRequest();
 });
 
-
+go_btn_year.addEventListener("click", function(){
+    sendYearRequest();
+});
 
 function sendNumberRequest():void {
     var val = usernum.value;
@@ -30,6 +35,29 @@ function sendNumberRequest():void {
     })
     .fail(function(error){
         num_result.innerHTML = "Something went horribly wrong";
+        console.log(error.getAllResponseHeaders());
+    });
+}
+
+function sendYearRequest():void {
+    var year = useryear.value;
+    $.ajax({
+        url: "https://numbersapi.p.mashape.com/"+year+"/year?fragment=true&json=false&notfound=floor",
+        beforeSend: function (xhrObj){
+            //Request Headers
+            xhrObj.setRequestHeader("X-mashape-Key","5CnggNUJoBmshhFFO5ojds013Dwap1pYGfIjsnIyJU7seBUXmv");
+            xhrObj.setRequestHeader("Accept","text/plaintext");
+        },
+        type: "GET",
+        dataType: "text"
+        
+    })
+    .done(function(data){
+        var json = JSON.parse(data);
+        year_result.innerHTML = "Is when " + json.text;
+    })
+    .fail(function(error){
+        year_result.innerHTML = "Something went horribly wrong";
         console.log(error.getAllResponseHeaders());
     });
 }
